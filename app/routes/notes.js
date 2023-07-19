3ab4d4f133af0d4e28fd3818520addf1a868aef3
Gleriston <xpanium@gmail.com>
2023-07-19 18:34:33 -0300

    Added in route, access permission

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const User = require('../models/user');
 const withAuth = require('../middlewares/auth');
 
@@ -15,5 +15,25 @@ router.post('/', withAuth, async function (req, res) {
     res.status(401).send(err);
   }
 });
+
+router.get('/:id', withAuth, async function (req, res) {
+  try {
+    const { id } = req.params;
+    let note = await Note.findById(id);
+    if (is_owner(req.user, note))
+      res.json(note);
+    else
+      res.status(403).json({ error: 'Permission denied' });
+  } catch (error) {
+    res.status(500).json({ error: 'Problem to get a note' })
+  }
+});
+
+const is_owner = (user, note) => {
+  if (JSON.stringify(user._id) == JSON.stringify(note.author._id))
+    return true;
+  else
+    return false;
+}
 
 module.exports = router;
