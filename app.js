d16a32a4def959f82f1ae2ee27a4b3ba6fe032bd
Gleriston <xpanium@gmail.com>
2023-07-19 19:19:55 -0300

    Include CORS

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 require('./config/database');
+const cors = require('cors');
 
-var usersRouter = require('./app/routes/users');
-var notesRouter = require('./app/routes/notes');
+const usersRouter = require('./app/routes/users');
+const notesRouter = require('./app/routes/notes');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
